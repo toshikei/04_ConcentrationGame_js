59f262f5f9d3ac2df86d8123c6d66a52800b485f
Rinako Fujisawa <morijyobi4153211@gmail.com>
2016-07-06 10:38:59 +0900

    格子状にトランプを配置

--- src/app.js
+++ src/app.js
@@ -1,27 +1,21 @@
 //gameScene.js
 var gameScene = cc.Scene.extend({
-	onEnter: function(){
-		this._super();
-		var gameLayer = new game();
-		this.addChild(gameLayer);
-  }
-	});
+    onEnter: function() {
+        this._super();
+        var gameLayer = new game();
+        this.addChild(gameLayer);
+    }
+});
 var game = cc.Layer.extend({
-        ctor: function() {
+    ctor: function() {
         this._super();
         var size = cc.director.getWinSize();
 
-        for (var i = 0; i < 5; i++) {
-            for (var j = 0; j < 5; j++) {
-                var sprite = new sprite(res.cover_png);
-                this.sprite.attr({
-                    x: size.height *0.1 + 56 * j,
-                    y: size.height *0.2 + 60 * i,
-                    scale: 1.0,
-                    rotation: 0
-                });
-                this.addChild(this.sprite, 0);
-            }
+        for (i = 0; i < 16; i++) {
+            var tile = cc.Sprite.create(res.cover_png);
+            this.addChild(tile, 0);
+            //タイルを格子状に配置する計算式
+            tile.setPosition(49 + i % 4 * 74, 400 - Math.floor(i / 4) * 74);
         }
     },
 });
