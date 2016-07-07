e6bfd4e5ef4cd42a7a2c107f4d2e7567aef608d2
Rinako Fujisawa <morijyobi4153211@gmail.com>
2016-07-07 11:18:32 +0900

    とりあえず完成

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@
 var gameScene = cc.Scene.extend({
     onEnter: function() {
         this._super();
-         var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
+        var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
         this.addChild(backgroundLayer);
 
         gameLayer = new game();
         this.addChild(gameLayer);
     }
 });
-
+//外部変数
 var console_label;
+var gameArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+var pickedTiles = [];
 var game = cc.Layer.extend({
     ctor: function() {
         this._super();
@@ -21,6 +23,8 @@ var game = cc.Layer.extend({
         for (i = 0; i < 16; i++) {
             //var tile = cc.Sprite.create(res.cover_png);
             var tile = new MemoryTile();
+            //pictureValueというプロパティを定義して、値を入れている
+            tile.pictureValue = gameArray[i];
             this.addChild(tile, 0);
             //タイルを格子状に配置する計算式
             tile.setPosition(49 + i % 4 * 74, 400 - Math.floor(i / 4) * 74);
@@ -36,11 +40,11 @@ var game = cc.Layer.extend({
 
 //Spriteクラスを拡張して実装してみた
 var MemoryTile = cc.Sprite.extend({
-    ctor:function() {
+    ctor: function() {
         this._super();
         this.initWithFile(res.cover_png);
         //イベントマネージャにイベントリスナーを追加
-        cc.eventManager.addListener(listener.clone(),this);
+        cc.eventManager.addListener(listener.clone(), this);
     },
 });
 
@@ -48,20 +52,43 @@ var MemoryTile = cc.Sprite.extend({
 var listener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
     swallowTouches: true,
-    onTouchBegan: function (touch, event) {
-      var target = event.getCurrentTarget();
-      var location = target.convertToNodeSpace(touch.getLocation());
-      var targetSize = target.getContentSize();
-      var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
+    onTouchBegan: function(touch, event) {
+        if (pickedTiles.length < 2) {
+            var target = event.getCurrentTarget();
+            var location = target.convertToNodeSpace(touch.getLocation());
+            var targetSize = target.getContentSize();
+            var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
 
-      if (cc.rectContainsPoint(targetRectangle, location)) {
-          console.log("I piced a tile!!");
-          console_label.setString("I piced a tile!!");
-          // 一秒後に消える
-       setTimeout(function() {
-           console_label.setString("");
-       }, 1500);
+            if (cc.rectContainsPoint(targetRectangle, location)) {
+                console.log("I piced a tile!!");
+                console_label.setString("pictueValue:" + target.pictureValue);
+                // 一秒後に消える
+                setTimeout(function() {
+                    console_label.setString("");
+                }, 1500);
 
-      }
-    }
+                //別の画像を割り当てる処理
+                target.initWithFile("res/tile_" + target.pictureValue + ".png");
+                pickedTiles.push(target);
+                if (pickedTiles.length == 2) {
+                    checkTiles();
+                }
+            }
+        }
+    },
 });
+
+function checkTiles() {
+  cc.log("checkTiles");
+  setTimeout(function() {
+      if(pickedTiles[0].pictureValue != pickedTiles[1].pictureValue){
+        pickedTiles[0].initWithFile(res.cover_png);
+        pickedTiles[1].initWithFile(res.cover_png);
+      }
+      else {
+        gameLayer.removeChild(pickedTiles[0]);
+        gameLayer.removeChild(pickedTiles[1]);
+      }
+      pickedTiles = [];
+  }, 1000);
+}
